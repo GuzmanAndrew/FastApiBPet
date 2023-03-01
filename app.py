a7991397e343fe583a02d8a7eb6f5845bba77f70
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2023-02-28 23:21:11 -0500

    add cors

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from routes.user import user
 from config.openapi import tags_metadata
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Users API",
     description="a REST API using python and mysql",
     version="0.0.1",
     openapi_tags=tags_metadata,
+)
+
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user)
